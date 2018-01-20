ba8fdb7550b7736726baf5e2f0d77531c71c9e83
Dmitry Balabka <dmitry.balabka@gmail.com>
2018-01-20 12:33:12 +0200

    Updated event libraries till latest versions

--- build/install.sh
+++ build/install.sh
@@ -11,7 +11,7 @@ apt-get update \
 && git clone https://github.com/torinaki/wrk.git && cd wrk && git checkout lua-plot-report \
 && make \
 && cd ../bin && rm -f wrk && cp ../wrk/wrk wrk && cd .. \
-&& git clone https://github.com/libuv/libuv.git && cd libuv && git checkout v1.11.0 \
+&& git clone https://github.com/libuv/libuv.git && cd libuv && git checkout v1.19.0 \
 && sh autogen.sh && ./configure && make `#&& make check` \
 && make install && cd .. \
 && git clone https://github.com/libevent/libevent.git && cd libevent && git checkout release-2.1.8-stable \
@@ -20,7 +20,7 @@ apt-get update \
 && docker-php-ext-install sockets \
 && docker-php-source extract \
 && pecl channel-update pecl.php.net \
-&& printf "\n\n\n\n\n\n\n" | pecl install ev-1.0.4 event-2.3.0 uv-0.1.2 \
+&& printf "\n\n\n\n\n\n\n" | pecl install ev-1.0.4 event-2.3.0 uv-0.2.2 \
 && docker-php-source delete \
 && php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
 && php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
