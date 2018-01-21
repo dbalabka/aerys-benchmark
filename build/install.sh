1387927b24219f597bfd5457171cc032da4f99f1
Dmitry Balabka <dmitry.balabka@gmail.com>
2018-01-21 14:20:55 +0200

    Fixed directory during instalation

--- build/install.sh
+++ build/install.sh
@@ -22,7 +22,7 @@ apt-get update \
 && pecl channel-update pecl.php.net \
 && printf "\n\n\n\n\n\n\n" | pecl install ev-1.0.4 event-2.3.0 \
 && git clone https://github.com/bwoebi/php-uv.git && cd ./php-uv \
-&& phpize && ./configure && make && make install \
+&& phpize && ./configure && make && make install && cd .. && rm -rf ./php-uv \
 && docker-php-source delete \
 && php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
 && php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
