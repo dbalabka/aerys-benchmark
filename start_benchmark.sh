2ff45f0e11b644bbbf3211f7ccd09272ec5fabb4
Dmitry Balabka <dmitry.balabka@gmail.com>
2018-02-13 16:17:50 +0200

    Added Aerys2 tiny implementation

--- start_benchmark.sh
+++ start_benchmark.sh
@@ -5,8 +5,8 @@ NODEJS_COMMAND='node'
 
 AERYS1_COMMAND="./aerys/vendor/bin/aerys -w 1 -c ./aerys/server.php --worker-args=\"-n\""
 AERYS_COMMAND="./aerys2/vendor/bin/aerys -w 1 -c ./aerys2/server.php --worker-args=\"-n\""
+AERYS_TINY_COMMAND="./aerys2/vendor/bin/aerys -w 1 -c ./aerys2/server-type.php --worker-args=\"-n\""
 AERYS1_WO_KEEP_ALIVE_COMMAND="./aerys/vendor/bin/aerys -w 1 -c ./aerys/server-wo-keep-alive.php --worker-args=\"-n\""
-AERYS_WO_TIMEOUT="./aerys2/vendor/bin/aerys -w 1 -c ./aerys2/server-wo-timeout.php --worker-args=\"-n\""
 REACTPHP_COMMAND="./react-php/server.php"
 NODEJS_SERVER_COMMAND="./nodejs/server.js"
 
@@ -62,6 +62,7 @@ php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
 && start_benchmark "Benchmarking Aerys + Amp1 (w/o keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS1_WO_KEEP_ALIVE_COMMAND}"
 start_benchmark "Benchmarking Aerys + Amp1 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS1_COMMAND}"
 start_benchmark "Benchmarking Aerys + Amp2 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND}"
+start_benchmark "Benchmarking Aerys + Amp2 tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_TINY_COMMAND}"
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
 && start_benchmark "Benchmarking Aerys + Amp2 (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_COMMAND}"
 start_benchmark "Benchmarking Aerys + Amp2 (keep-alive + OPCache + ev)" "${PHP_COMMAND} -c ./php/default-opcache-ev.ini ${AERYS_COMMAND}"
