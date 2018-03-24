869b1e883c11ef6712e148648e0adb262fc16fea
Dmitry Balabka <dmitry.balabka@gmail.com>
2018-03-24 14:53:52 +0200

    Fixed composer install for Aerys

--- build/install.sh
+++ build/install.sh
@@ -34,8 +34,9 @@ apt-get update \
 && php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
 && php composer-setup.php \
 && php -r "unlink('composer-setup.php');" \
-&& php composer.phar install -o -d ./aerys \
-&& php composer.phar install -o -d ./aerys2 \
+&& php composer.phar install -o -d ./aerys/v0.5.0 \
+&& php composer.phar install -o -d ./aerys/v0.7.4 \
+&& php composer.phar install -o -d ./aerys/v0.8 \
 && php composer.phar install -o -d ./react-php \
 && chmod +x ./start_benchmark.sh \
 && rm -rf ./wrk \
