c821efbc4723ab360adc4fcd97eabc0eb114affe
Dmitry Balabka <dmitry.balabka@gmail.com>
2019-02-18 16:29:26 +0200

    Added PHP 7.4. Made PHP 8 as allowed failure

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-docker build -t aerys-benchmark:php71 -f ./build/php71/Dockerfile . \
-&& docker build -t aerys-benchmark:php72 -f ./build/php72/Dockerfile . \
-&& docker build -t aerys-benchmark:php73 -f ./build/php73/Dockerfile . \
-&& docker build -t aerys-benchmark:php8jit -f ./build/php8jit/Dockerfile .
+# Print commands and their arguments as they are executed.
+set -x
+# Exit on any error
+set -e
+
+docker build -t aerys-benchmark:php71 -f ./build/php71/Dockerfile .
+docker build -t aerys-benchmark:php72 -f ./build/php72/Dockerfile .
+docker build -t aerys-benchmark:php73 -f ./build/php73/Dockerfile .
+docker build -t aerys-benchmark:php74jit -f ./build/php74jit/Dockerfile .
+docker build -t aerys-benchmark:php8jit -f ./build/php8jit/Dockerfile . || echo "Skip PHP 8. Docker image build fail!"
