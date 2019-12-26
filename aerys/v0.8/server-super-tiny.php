c44439cb56888a15ddd80d3e87247a1b471c4125
Dmitry Balabka <dmitry.balabka@gmail.com>
2019-12-26 13:34:50 +0200

    Added super tiny version of HTTP version w/o HTTP parsing. Removed old code.

---
+++ aerys/v0.8/server-super-tiny.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+/*
+ * (c) 2018, Dmitrijs Balabka
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+// This is a very simple HTTP server that just prints a message to each client that connects.
+// It doesn't check whether the client sent an HTTP request.
+
+// You might notice that your browser opens several connections instead of just one, even when only making one request.
+
+use Amp\Loop;
+use Amp\Socket\ServerSocket;
+use function Amp\asyncCoroutine;
+
+Loop::run(function () {
+    $requestCount  = 0;
+    $clientHandler = asyncCoroutine(function (ServerSocket $socket) use (&$requestCount) {
+        list($ip, $port) = explode(":", $socket->getRemoteAddress());
+
+        $buffer = '';
+        while (($chunk = yield $socket->read()) !== null) {
+            $buffer .= $chunk;
+            if (\substr($buffer, -4, 4) === "\r\n\r\n") {
+                $date       = \gmdate("D, d M Y H:i:s", \time()) . " GMT";
+                $body       = "Hello world!";
+                $bodyLength = \strlen($body);
+                $requestCount++;
+                echo $requestCount;
+                yield $socket->write("HTTP/1.1 200 OK\r\nContent-Type: text/plain; Charset=utf-8\r\nX-Powered-By: AerysServer\r\nConnection: keep-alive\r\nContent-Length: ${bodyLength}\r\nKeep-Alive: timeout=10000\r\nDate: ${date}\r\n\r\n${body}\r\n\r\n\r\n\r\n");
+            }
+        }
+    });
+
+    $server = Amp\Socket\listen("0.0.0.0:8080");
+
+    echo "Listening for new connections on " . $server->getAddress() . " ..." . PHP_EOL;
+
+    while ($socket = yield $server->accept()) {
+        $clientHandler($socket);
+        if ($requestCount > 4) {
+            exit;
+        }
+    }
+});
--- start_benchmark.sh
+++ start_benchmark.sh
@@ -7,12 +7,10 @@ PHP_COMMAND="php -n"
 NODEJS_COMMAND='node'
 
 # Amp HTTP server
-AERYS1_COMMAND="./aerys/v0.5.0/vendor/bin/aerys -w 1 -c ./aerys/v0.5.0/server.php --worker-args=\"-n\""
-AERYS1_WO_KEEP_ALIVE_COMMAND="./aerys/v0.5.0/vendor/bin/aerys -w 1 -c ./aerys/v0.5.0/server-wo-keep-alive.php --worker-args=\"-n\""
-# TODO: update with after server.php will be updated till latest cluster version
-#AERYS_COMMAND="./aerys2/vendor/bin/aerys -w 1 -c ./aerys2/server.php --worker-args=\"-n\""
-AERYS_COMMAND="./aerys/version/server-tiny.php"
-AERYS_TINY_COMMAND="./aerys/version/server-tiny.php"
+AERYS_COMMAND="./aerys/version/vendor/bin/aerys -w 1 -c ./aerys/version/server.php --worker-args=\"-n\""
+AERYS_WO_KEEP_ALIVE_COMMAND="./aerys/version/vendor/bin/aerys -w 1 -c ./aerys/version/server-wo-keep-alive.php --worker-args=\"-n\""
+AMP_HTTP_SERVER_TINY_COMMAND="./aerys/version/server-tiny.php"
+AMP_HTTP_SERVER_SUPER_TINY_COMMAND="./aerys/version/server-super-tiny.php"
 AERYS_CURRENT_VERSION="v1.x"
 
 SWOOLE_COMMAND="./swoole/server.php"
@@ -75,34 +73,34 @@ start_benchmark "Benchmarking ReactPHP (w/o keep-alive + OPCache)" "${PHP_COMMAN
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
 && start_benchmark "Benchmarking ReactPHP (w/o keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${REACTPHP_COMMAND}"
 
-start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive)" "${PHP_COMMAND} -c ./php/default.ini ${AERYS1_WO_KEEP_ALIVE_COMMAND}"
-start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS1_WO_KEEP_ALIVE_COMMAND}"
+start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive)" "${PHP_COMMAND} -c ./php/default.ini ${AERYS_WO_KEEP_ALIVE_COMMAND//version/v0.5.0}"
+start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_WO_KEEP_ALIVE_COMMAND//version/v0.5.0}"
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
-&& start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS1_WO_KEEP_ALIVE_COMMAND}"
-start_benchmark "Benchmarking Aerys v0.5.0 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS1_COMMAND}"
+&& start_benchmark "Benchmarking Aerys v0.5.0 (w/o keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_WO_KEEP_ALIVE_COMMAND//version/v0.5.0}"
+start_benchmark "Benchmarking Aerys v0.5.0 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND//version/v0.5.0}"
 
-start_benchmark "Benchmarking Aerys v0.7.4 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND//version/v0.7.4}"
-start_benchmark "Benchmarking Aerys v0.7.4 tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_TINY_COMMAND//version/v0.7.4}"
+start_benchmark "Benchmarking Aerys v0.7.4 tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND//version/v0.7.4}"
+start_benchmark "Benchmarking Aerys v0.7.4 super tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AMP_HTTP_SERVER_SUPER_TINY_COMMAND//version/v0.7.4}"
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
-&& start_benchmark "Benchmarking Aerys v0.7.4 (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_COMMAND//version/v0.7.4}"
+&& start_benchmark "Benchmarking Aerys v0.7.4 tiny (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_COMMAND//version/v0.7.4}"
 
-start_benchmark "Benchmarking Aerys v0.8 (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND//version/v0.8}"
-start_benchmark "Benchmarking Aerys v0.8 tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_TINY_COMMAND//version/v0.8}"
+start_benchmark "Benchmarking Aerys v0.8 tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/v0.8}"
+start_benchmark "Benchmarking Aerys v0.8 super tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AMP_HTTP_SERVER_SUPER_TINY_COMMAND//version/v0.8}"
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
-&& start_benchmark "Benchmarking Aerys v0.8 (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_COMMAND//version/v0.8}"
+&& start_benchmark "Benchmarking Aerys v0.8 tiny (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/v0.8}"
 
-start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_COMMAND//version/${AERYS_CURRENT_VERSION}}"
-start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AERYS_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} super tiny (keep-alive + OPCache)" "${PHP_COMMAND} -c ./php/default-opcache.ini ${AMP_HTTP_SERVER_SUPER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
 php -n -c ./php/default-opcache.ini -i | grep "opcache.jit" 1>/dev/null \
-&& start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AERYS_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+&& start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache + w/o JIT)" "${PHP_COMMAND} -c ./php/default-opcache-nojit.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
 # TODO: Aerys doesn't work with EV extension on PHP 7.3
 if php -v | egrep -q "^PHP 7.(1|2|3)"; then
-    start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} (keep-alive + OPCache + ev)" "${PHP_COMMAND} -c ./php/default-opcache-ev.ini ${AERYS_COMMAND//version/${AERYS_CURRENT_VERSION}}"
-    start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} (keep-alive + OPCache + event)" "${PHP_COMMAND} -c ./php/default-opcache-event.ini ${AERYS_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+    start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache + ev)" "${PHP_COMMAND} -c ./php/default-opcache-ev.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+    start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache + event)" "${PHP_COMMAND} -c ./php/default-opcache-event.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
 else
     echo "Skip Ev and Event benchmark for current version of PHP"
 fi
-start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} (keep-alive + OPCache + uv)" "${PHP_COMMAND} -c ./php/default-opcache-uv.ini ${AERYS_COMMAND//version/${AERYS_CURRENT_VERSION}}"
+start_benchmark "Benchmarking Aerys ${AERYS_CURRENT_VERSION} tiny (keep-alive + OPCache + uv)" "${PHP_COMMAND} -c ./php/default-opcache-uv.ini ${AMP_HTTP_SERVER_TINY_COMMAND//version/${AERYS_CURRENT_VERSION}}"
 
 # Benchmark swoole
 start_benchmark "Benchmarking Swoole (keep-alive + OPCache + swoole)" "${PHP_COMMAND} -c ./php/default-opcache-swoole.ini ${SWOOLE_COMMAND}"
